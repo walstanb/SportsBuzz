486774ee7146a226ada5c9615d313235283767ac
ninadchoksi <49779835+ninadchoksi@users.noreply.github.com>
2020-08-23 16:31:52 +0530

    pagination alpha

--- application/routes.py
+++ application/routes.py
@@ -3,11 +3,18 @@ from flask import render_template, flash, redirect, request
 from application.forms import *
 from application.models import *
 from datetime import date
-
+pagecounter = 0
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    results = db.team_schedule.find()
+    global pagecounter
+    skips=0
+    number=5
+    if (pagecounter+number>1):
+        skips = 11 * (pagecounter+number)
+        pagecounter += number
+
+    results = db.team_schedule.find().skip(skips).limit(11)
     return render_template("football.html", results = results)
 
 
