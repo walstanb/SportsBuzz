8ee5adcff1076f3dd5b33a3c39925fb3b4e3cc2e
Walstan Baptista <38958184+walstanb@users.noreply.github.com>
2020-08-26 18:39:49 +0530

    fix: merge conflicts resolved mannually

--- application/routes.py
+++ application/routes.py
@@ -35,4 +35,20 @@ def schedule(id=0):
     print (pages)
     return render_template("football.html", results = results,i=pagecounter,p=pages)
 
+@app.route("/football_teams", methods=['GET', 'POST'])
+@app.route("/football_teams/<tab>", methods=['GET', 'POST'])
+def football_teams(tab="leagues"):
+    player_stats=[]
+    if tab=="leagues":
+        [player_stats.append(x) for x in db.epl20playerdata.find()]
+    elif tab=="international":
+        [player_stats.append(x) for x in db.epl20playerdata.find()]
+    teamdict={}
+    for x in player_stats:
+        if x['Club'] not in teamdict.keys():
+            teamdict[x['Club']]="f"+str(id(x['Club']))
+    rp=list(teamdict.keys())
+    teamz=rp[0]
+    
+    return render_template("playerstats-football.html",player_stats=player_stats, teamdict=teamdict, teamz=teamz, tab=tab)
 
